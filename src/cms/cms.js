da3eed47250ecb93853718b68ac553e896ecf691
Mattias Ravand <mravand@Mattiass-iMac.lan>
2019-01-26 19:01:27 +0100

    SeoPagePreview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -3,7 +3,9 @@ import CMS from 'netlify-cms'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
+import SeoPagePreview from './preview-templates/SeoPagePreview'
 
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
+CMS.registerPreviewTemplate('seo', SeoPagePreview)
